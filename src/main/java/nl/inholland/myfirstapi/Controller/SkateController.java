4aed7171d2e16f5c4d099c1c916e96ec8e459c4f
thijsvtol <43065692+thijsvtol@users.noreply.github.com>
2020-04-20 20:25:16 +0200

    opdracht les 2 (filter/parameter)

--- src/main/java/nl/inholland/myfirstapi/Controller/SkateController.java
+++ src/main/java/nl/inholland/myfirstapi/Controller/SkateController.java
@@ -3,24 +3,56 @@ package nl.inholland.myfirstapi.Controller;
 import nl.inholland.myfirstapi.Model.Skate;
 import nl.inholland.myfirstapi.Service.SkateService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("skates")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost"} )
+@CrossOrigin(origins = "http://localhost:8080")
 public class SkateController {
 
     @Autowired private SkateService service;
 
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity getAllSkates() {
-        List<Skate> skates = service.getAllSkates();
-        return ResponseEntity.status(200).body(skates);
+        return ResponseEntity.status(200).body(service.getAllSkates());
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getSkateById(@PathVariable("id") Integer id) {
+        for(Skate skate : service.getAllSkates()){
+            if(skate.getId() == id){
+                return ResponseEntity.status(200).body(skate);
+            }
+        }
+        return ResponseEntity.status(404).body("could not find a skate with id: " + id);
+    }
+
+    @RequestMapping(value = "/nameStartsWith/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getSkateName(@PathVariable("name") String name) {
+        //Get first skate name that begins with the given parameters
+        List<String> skateNames = new ArrayList<>();
+
+        //Get all skate names
+        for (Skate skate : service.getAllSkates()) {
+            skateNames.add(skate.getName());
+        }
+
+        //Return first result
+        return ResponseEntity.status(404).body(skateNames
+                .stream()
+                .filter(a -> a.startsWith(name))
+                .findFirst()
+                .orElse("No result"));
     }
 }
